1042b909084d09e7d0caf564a129bb2ea8a939c6
jerrychang22 <jerrychang22@gmail.com>
2016-12-02 06:34:51 -0500

    Fix some more bugs

--- parse.c
+++ parse.c
@@ -31,27 +31,41 @@ void parseParams(){
     
   //Removes the newline for interpreting
   *(strstr(line, "\n")) = 0;
-  
+
   //Split all the commands by ;
   char **params = semiBreak(line);
   
   //Loop through all ,fork and wait for child processes
   int status;
   while(*params){ //While there are commands
+    printf("Check 2\n");
+      
     char *commands[256];
     //Split each line of commands by space and parse
     //Make sure each command string is stripped of whitespace before
     //splitting by spaces
     int i = 0;
-    strip(*params);
-    printf("%c\n", **params);
+    *params = strip(*params);
+    printf("Check 3\n");
     while(*params){
       commands[i] = strsep(&*params, " ");
+      //printf("Command : %s", commands[i]);
       i++;
       //printf("num args : %d\n", i);
     }
     commands[i] = 0;
-    
+   
+    //Builtins
+    /*
+    if (strcmp(commands[0], "exit") == 0)
+        exit(0);
+    if (strcmp(commands[0], "cd") == 0){
+        if (i == 1) chdir("~");
+        else chdir(commands[1]);
+        return;
+    }
+    //*/
+
     //fork and exec here
     if (fork() == 0){
         execvp(commands[0], commands);
@@ -85,6 +99,7 @@ char **semiBreak(char *str){
     int i = 0;
     for(i; commands[i] = strsep(&str, ";"); i++);
     commands[i] = 0;
+    printf("Check 1\n");
 
     return ans;
 }
@@ -97,17 +112,18 @@ char **semiBreak(char *str){
 //
 //Moves pointer to first non-whitespace character and writes a terminating
 //null after last non-whitespace character
-void strip(char *str){
+char *strip(char *str){
     
     //Move pointer to front of leading whitespace
     //isspace checks for spaces, tabs, and newlines (all whitespace)
     while(isspace(*str)) str++;
-    if (!str) return;
+    if (str == NULL) return;
 
     //Make a pointer at the end of the string
     //- 1 for terminating null
     char *ending = str + strlen(str) - 1;
     while (isspace(*ending)) ending--;
     *(ending + 1) = 0;  //Write in new terminating null
-
+    
+    return str;
 }
